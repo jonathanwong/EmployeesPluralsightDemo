f9683ae987d96a84b8daf98511a8c619a76f1419
Jonathan Wong <jonwong@outlook.com>
2024-03-02 13:41:34 -0800

    Initial Commit

---
+++ EmployeesPluralsightDemo/EmployeesPluralsightDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeesPluralsightDemoApp.swift
+//  EmployeesPluralsightDemo
+//
+//  Created by Jonathan Wong on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct EmployeesPluralsightDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
